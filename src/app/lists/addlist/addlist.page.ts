83b12f47d97c13b3dbf5ce3baa08d2a945a7de0a
Amine <jonahi.amine@gmail.com>
2020-08-30 20:22:14 +0200

    add list code

--- src/app/lists/addlist/addlist.page.ts
+++ src/app/lists/addlist/addlist.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { List } from 'src/app/shared/list.model';
+import { ListService } from 'src/app/service/list.service';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-addlist',
@@ -16,9 +19,24 @@ export class AddlistPage implements OnInit {
     Writers: []
   }
 
-  constructor() { }
+  constructor(
+    private listService: ListService,
+    private router: Router,
+    private fireauth: AngularFireAuth,
+  ) { }
 
   ngOnInit() {
+    this.fireauth.currentUser.then(
+      currentUser => {
+        this.list.owner = currentUser.email;
+      });
+  }
+
+  addList() {
+    this.listService.addList(this.list).then(
+      () => {
+        this.router.navigate(['lists']);
+      });
   }
 
 }
