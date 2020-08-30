ac005d0716fdf26c2066bdd22b267281ba96ab59
Amine <jonahi.amine@gmail.com>
2020-08-30 20:19:00 +0200

    show list

--- src/app/lists/lists.page.ts
+++ src/app/lists/lists.page.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import 'firebase/auth';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { combineLatest, Subscription } from 'rxjs';
+import { ListService } from '../service/list.service';
+import { List } from '../shared/list.model';
 
 @Component({
   selector: 'app-lists',
@@ -6,7 +12,37 @@ import { Component } from '@angular/core';
   styleUrls: ['lists.page.scss'],
 })
 export class ListsPage {
+  private Subscription: Subscription;
+  list: List[] = [];
 
-  constructor() {}
+  constructor(
+    private router: Router,
+    private listService: ListService,
+    private fireauth: AngularFireAuth,
+  ) {}
 
+  ionViewWillEnter() {
+    this.fireauth.currentUser.then(
+      currentUser => {
+        this.Subscription = combineLatest(
+          this.listService.getOwnerList(currentUser.email),
+          this.listService.getAllowedToReadList(currentUser.email),
+          this.listService.getAllowedToWriteList(currentUser.email)
+        ).subscribe(
+          data => {
+            this.list = data[0].concat(data[1], data[2]);
+          });
+      });
+  }
+
+  onAdd() {
+    this.router.navigateByUrl('/lists/addlist');
+  }
+
+  ionViewWillLeave() {
+    if(this.Subscription){
+      this.Subscription.unsubscribe();
+    }
+  }
+  
 }
