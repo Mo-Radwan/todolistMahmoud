f75f90b6acb0e975bd43b56ef12ab4257429dbf0
Amine <jonahi.amine@gmail.com>
2020-08-30 18:54:36 +0200

    services

---
+++ src/app/service/list.service.ts
@@ -0,0 +1,134 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+import { List } from '../shared/list.model';
+import { Observable } from 'rxjs';
+import { Item } from '../shared/item.model';
+import { map,take } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ListService {
+
+
+  private ListCollection: AngularFirestoreCollection<List>;
+  private List$: Observable<List[]>;
+
+  constructor(private afs: AngularFirestore) {
+
+    this.ListCollection = afs.collection<List>('List');
+
+    this.List$ = this.ListCollection.snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        });
+      })
+    );
+
+  }
+
+  addList(List: List) {
+    return this.ListCollection.add(List);
+  }
+
+  deleteList(List: List) {
+    this.getListItems(List.id).pipe(take(1)).subscribe(
+      fetchedItems => {
+        fetchedItems.map(item => this.deleteItem(List.id,item));
+      });
+      return this.ListCollection.doc(List.id).delete();
+  }
+
+  updateList(List: List) {
+    return this.ListCollection.doc(List.id)
+      .set(
+        {
+          title: List.title,
+          body: List.body,
+          allowedToRead: List.Readers,
+          allowedToWrite: List.Writers
+        },
+        {
+          merge: true
+        });
+  }
+
+  getLists() {
+    return this.List$;
+  }
+
+  getList(id: string) {
+    return this.ListCollection
+      .doc<List>(id)
+      .valueChanges()
+      .pipe(
+        map(data => {
+          return { id, ...data };
+        })
+      );
+  }
+
+  getListItems(idl: string): Observable<Array<Item>> {
+    return this.ListCollection
+      .doc<List>(idl)
+      .collection<Item>('items')
+      .valueChanges({ idField: 'id' })
+      .pipe(
+        map(data => {
+          return data.map(dataEl => {
+            return { idl, ...dataEl }
+          });
+        })
+      );
+  }
+
+  getAllowedToReadList(userEmail: string) {
+    return this.afs.collection<List>('List', ref => ref.where('Readers', 'array-contains', userEmail)).valueChanges({ idField: 'id' });
+  }
+
+  getAllowedToWriteList(userEmail: string) {
+    return this.afs.collection<List>('List', ref => ref.where('Writers', 'array-contains', userEmail)).valueChanges({ idField: 'id' });
+  }
+
+  getOwnerList(userEmail: string) {
+    return this.afs.collection<List>('List', ref => ref.where('owner', '==', userEmail)).valueChanges({ idField: 'id' });
+  }
+
+  addItem(ListId: string, item: Item) {
+    return this.ListCollection.doc(ListId).collection('items').add(item);
+  }
+
+  deleteItem(ListId: string, item: Item) {
+    return this.ListCollection.doc(ListId).collection('items').doc(item.id).delete();
+  }
+
+  updateItem(ListId: string, item: Item) {
+    return this.ListCollection.doc(ListId).collection('items').doc(item.id)
+      .set(
+        {
+          title: item.title,
+          body: item.body,
+          isDone: item.isDone
+        },
+        {
+          merge: true
+        });
+  }
+
+  getItem(ListId: string, itemId: string): Observable<Item> {
+    return this.ListCollection
+      .doc<List>(ListId)
+      .collection('items')
+      .doc<Item>(itemId)
+      .valueChanges()
+      .pipe(
+        map(data => {
+          return { id: itemId, idl: ListId, ...data };
+        })
+      );
+  }
+
+}
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,93 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+import { User } from '../shared/user.model';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+
+  private _usersCollection: AngularFirestoreCollection<User>;
+  private _users$: Observable<User[]>;
+
+  constructor(private db: AngularFirestore) {
+    this._usersCollection = db.collection<User>('users');
+
+    this._users$ = this._usersCollection.snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(
+          a => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc.id;
+            return { id, ...data };
+          }
+        );
+      })
+    );
+  }
+
+  getUsers(): Observable<Array<User>> {
+    return this._users$;
+  }
+
+  addUser(user: User) {
+    return this._usersCollection.add(user);
+  }
+
+  deleteUser(user: User) {
+    return this._usersCollection.doc(user.id).delete();
+  }
+
+  getUser(uid: string): Promise<User> {
+    return this._usersCollection.ref.where('uid', '==', uid).get().then(
+      querySnapshot => {
+        let tmpUser: User = null;
+        querySnapshot.forEach(doc => {
+          if (doc.data()) {
+            tmpUser = doc.data() as User;
+            tmpUser.id = doc.id;
+          }
+        });
+        return tmpUser;
+      },
+      error => {
+        throw error;
+      }
+    );
+  }
+
+  getUserByEmail(email: string): Promise<User> {
+    return this._usersCollection.ref.where('email', '==', email).get().then(
+      querySnapshot => {
+        let tmpUser: User = null;
+        querySnapshot.forEach(doc => {
+          if (doc.data()) {
+            tmpUser = doc.data() as User;
+            tmpUser.id = doc.id;
+          }
+        });
+        return tmpUser;
+      },
+      error => {
+        throw error;
+      }
+    );
+  }
+
+  update(user: User) {
+    return this._usersCollection.doc(user.id)
+      .set(
+        {
+          id: user.id,
+          uid: user.uid,
+          fullName: user.fullName,
+          email: user.email,
+        },
+        {
+          merge: true
+        });
+  }
 }
